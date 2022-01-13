5b2e5cf48f7890b3b57890f736ce2fb187ecd250
irahulmalik <rahulmalik1995@gmail.com>
2022-01-13 15:56:25 +0530

    added dotenv

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -5,34 +5,35 @@ import { User } from "../entity/user_entity";
 import bcryptjs from 'bcryptjs';
 import { sign } from "jsonwebtoken";
 
-
+//register user function
 export const register = async (req:Request, res:Response) => {
     const body = req.body;
-
+    //check if both password provided are same
     if(body.password != body.confirmPassword){
         return res.status(400).send({
             message:"Passwords does not match"
         })
         
     }
-        const user = await getRepository(User).save({
-            firstName : body.firstName,
-            last_name : body.lastName,
-            email: body.email,
-            password: await bcryptjs.hash(body.password, 10),
-            is_ambassdor:false
-        });
-    
+    //saves user and returns password in one field and user in other
+    const {password, ...user} = await getRepository(User).save({
+        firstName : body.firstName,
+        last_name : body.lastName,
+        email: body.email,
+        password: await bcryptjs.hash(body.password, 10),
+        is_ambassdor:false
+    });
+    //return back user
     res.send(user)
 }
-
+//login functionality
 export const login = async(req:Request, res:Response) =>{
     const user = await getRepository(User).findOne({email:req.body.email})
-
+    //validating if user even exist
     if(!user){
         return res.status(400).send({message:"invalid credentials"})
     }
-    
+    //validating password
     if(!bcryptjs.compare(req.body.password, user.password)){
         return res.status(400).send({message:"invalid credentials"})
     }
@@ -41,5 +42,15 @@ export const login = async(req:Request, res:Response) =>{
         id: user.id,
     },"secret"
     )
-    res.send(token)
+
+    //storing json token in cookie
+    res.cookie("jwt",token,(
+        {
+            httpOnly:true, 
+            maxAge:24 * 60 * 60 * 1000//1 day
+        }
+        ))
+
+        //sending success message
+    res.send({message: "success"})
 }
